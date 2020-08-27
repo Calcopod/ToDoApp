cb0fd661fb2d5402f46caf9cb4714f457e1942cc
Rosca Serban <rosca.serban2@gmail.com>
2020-08-27 19:29:53 +0300

    Updates

---
+++ precache-manifest.abc7b4cc889b1e7b3d01d905774c20c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f42ecaa7c8e7ec3d05f802a37905f951",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "595c05e649711bb0dcae",
+    "url": "/ToDoApp/static/css/main.57b43e26.chunk.css"
+  },
+  {
+    "revision": "b57650b8184e68490f91",
+    "url": "/ToDoApp/static/js/2.d4fefe6b.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/ToDoApp/static/js/2.d4fefe6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "595c05e649711bb0dcae",
+    "url": "/ToDoApp/static/js/main.a4bfc4c3.chunk.js"
+  },
+  {
+    "revision": "4f1a562c4c892a727e9e",
+    "url": "/ToDoApp/static/js/runtime-main.25b0f610.js"
+  }
+]);
